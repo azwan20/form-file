08121fadcd58f5cd023f354c8e0edcf9b323623d
azwan20 <triyadi20112001@gmail.com>
2024-01-27 14:46:19 +0800

    chnge

--- src/pages/api/file/route.ts
+++ src/pages/api/file/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
-  const filename = searchParams.get('filename') || "";
+  const filename = searchParams.get('filename') || '';
 
   if(filename && request.body){
 
